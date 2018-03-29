3f892085b5233584a300aefaa4fe45b92bf5dfda
Stan Kleshnov <s.v.kleshnov@gmail.com>
2018-03-30 00:06:24 +0300

    initial

--- js/index.js
+++ js/index.js
@@ -1,10 +1,6 @@
 const a = prompt('Ввести A');
 const b = prompt('Ввести B');
 const c = prompt('Ввести C');
-
-const result = solveQuadr(a, b, c);
-document.write ('result');
-
 function solveQuadr(a, b, c) {
   let
   D, x1, x2;
@@ -14,15 +10,18 @@ function solveQuadr(a, b, c) {
     x1 = (-b + Math.sqrt(D))/(2*a);
     x2 = (-b - Math.sqrt(D))/(2*a);
     alert('x1 = '+x1+'  x2 = '+x2);
-  }
+   }
   else if (D == 0) {
     x1 = (-b)/(2*a);
     alert('x =' +x1);
-  }
+   }
   else if (D < 0) {
     alert('Не має дійсних коренів, тому що дискримінант менший за нуль.')
-  }
+   }
 }
+
+const result = solveQuadr(a, b, c);
+document.write ('Така от відповідь!!!');
 
 // function functionName() {
 //   alert('functionName')
